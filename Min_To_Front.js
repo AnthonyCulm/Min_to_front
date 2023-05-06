281e4b6537b89af62fcb489bd927c80a6ce39e2f
AnthonyCulm <Anthonyculm@yahoo.com>
2023-05-05 23:19:54 -0400

    Fixed the order

--- Min_To_Front.js
+++ Min_To_Front.js
@@ -2,9 +2,10 @@ var arr = [4, 2, 1, 3, 5]
 function min_to_max() {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] == 1) {
-            var temp = arr[2];
-            arr[2] = arr[0];
-            arr[0] = temp;
+            var temp = arr[i];
+            arr[i]=temp
+            delete arr[i];
+            arr.unshift(temp);
         }
 
 
